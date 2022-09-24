a57874b99062eb71776b10922baba2b77db056c9
gokul0317 <gokulkrishnan.p2@gmail.com>
2022-09-24 12:35:54 +0530

    updated proxy

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { addWallet, addTransaction, getAllTransactions, gettransactionById } = re
 const path = require("path");
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 app.use(express.json());
 
 // Post req.body => name: string, balance: number;
